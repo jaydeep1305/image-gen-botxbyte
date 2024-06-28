182c30e0dcc4f10d5632d7d12760743a93f7e187
jaydeep <jaydeep1305>
2024-06-28 12:36:47 +0530

    port 5000

--- app.py
+++ app.py
@@ -555,5 +555,5 @@ def generate_single_image():
     return {"success": False, "message": "Unexpected Errors."}
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5000)
 
